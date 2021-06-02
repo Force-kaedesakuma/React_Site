664ff63f06835c9808f10e34d2d0026f6f635eb0
Force-kaedesakuma <kaede.sakuma@force-corp.co.jp>
2021-06-02 23:39:28 +0900

    Initial commit

---
+++ src/js/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <header class="header">
+      <div class="header-inner">
+      <h1 class="logo"><a href="#">MyProfile</a></h1>
+      <div class="menu-wrapper">
+      </div>
+      </div>
+    </header>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App" id="root">
+//       Test
+//     </div>
+//   );
+// }
+
+export default App;
